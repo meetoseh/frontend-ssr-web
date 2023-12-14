88a83bfb232ab9f94d2774f0507cda902f73352d
Timothy Moore <mtimothy984@gmail.com>
2023-12-14 12:01:22 -0800

    Capture stderr in tee

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,4 +2,5 @@
 source /root/.bashrc
 source /root/.nvm/nvm.sh
 source /home/ec2-user/config.sh
-npx ts-node --experimental-specifier-resolution=node --esm build/server/server.bundle.js --host 0.0.0.0 --port 80 --reuse-artifacts | tee >(split --additional-suffix=.log -d -b 1000000 - debug.0)
+rm debug.*.log
+npx ts-node --experimental-specifier-resolution=node --esm build/server/server.bundle.js --host 0.0.0.0 --port 80 --reuse-artifacts 2>&1 | tee >(split --additional-suffix=.log -d -b 1000000 - debug.0)
